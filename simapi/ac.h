1d188c0838dfdceb1e18694bbf4ccc8b61c0e88d
Paul Dino Jones <paul@spacefreak18.xyz>
2023-02-18 15:07:04 +0000

    Adding CrewChief plugin shared memory

--- simapi/ac.h
+++ simapi/ac.h
@@ -7,18 +7,22 @@
 #define AC_PHYSICS_FILE "acpmf_physics"
 #define AC_STATIC_FILE "acpmf_static"
 #define AC_GRAPHIC_FILE "acpmf_graphic"
+#define AC_CREWCHIEF_FILE "acpmf_crewchief"
 
 typedef struct
 {
     bool has_physics;
     bool has_static;
     bool has_graphic;
+    bool has_crewchief;
     void* physics_map_addr;
     void* static_map_addr;
     void* graphic_map_addr;
+    void* crewchief_map_addr;
     struct SPageFilePhysics ac_physics;
     struct SPageFileStatic ac_static;
     struct SPageFileGraphic ac_graphic;
+    struct SPageFileCrewChief ac_crewchief;
 }
 ACMap;
 
