1d8380d5dc560df81d27faa1f75643e72e705081
Paul Dino Jones <paul@spacefreak18.xyz>
2025-01-13 00:52:35 -0500

    Fix console spam

--- simd/simd.c
+++ simd/simd.c
@@ -206,14 +206,12 @@ void datacheckcallback(uv_timer_t* handle)
 
     if ( appstate == 1 )
     {
-        fprintf(stderr, "calling getsim");
         SimInfo si = getSim(simdata, simmap, false, startudp, true);
         //TODO: move all this to a siminfo struct in loop_data
         f->simstate = si.isSimOn;
         f->sim = si.simulatorapi;
         f->use_udp = si.SimUsesUDP;
 
-        fprintf(stderr, "simstate is %i", f->simstate);
     }
     if (f->simstate == true && simdata->simstatus >= 2)
     {
@@ -234,7 +232,6 @@ void datacheckcallback(uv_timer_t* handle)
             }
             else
             {
-                fprintf(stderr, "map timer start");
                 uv_timer_start(&datamaptimer, shmdatamapcallback, 2000, 16);
             }
             uv_timer_stop(handle);
