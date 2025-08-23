2ae1fe83e9ef17965970446e2a8bec6d6f25dafb
Paul Dino Jones <paul@spacefreak18.xyz>
2025-08-23 13:34:10 -0400

    fix log message when env vars aren't found

--- simd/simd.c
+++ simd/simd.c
@@ -288,7 +288,7 @@ void gamefindcallback(uv_timer_t* handle)
             if(env1 == NULL || env2 == NULL || env3 == NULL)
             {
                 err = -1;
-                y_log_message(Y_LOG_LEVEL_WARNING, "Could not find one or all of the necessary environment variables. Found %s %s %s");
+                y_log_message(Y_LOG_LEVEL_WARNING, "Could not find one or all of the necessary environment variables. Found %s %s %s", env1, env2, env3);
             }
 
             if(err == 0)
