43d3ef06624b267ee1d7bd91640857565d777730
Paul Dino Jones <paul@spacefreak18.xyz>
2025-11-16 16:20:04 -0500

    update simdata mapper

--- simmap/basicmap.h
+++ simmap/basicmap.h
@@ -2,11 +2,14 @@
 #define _BASICMAP_H
 
 #include "../simapi/simapi.h"
+#include "../simapi/simdata.h"
 
 struct Map {
     const char* name;
     void* value;
     SimDataType dtype;
 };
+
+int CreateSimDataMap(struct Map *map, SimData *simdata, int mapdata);
 
 #endif
